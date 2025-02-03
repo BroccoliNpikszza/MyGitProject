a0befe26b7a887eedc4cbb5069aaf3d0712d7735
mademedothis <138303668+mademedothis@users.noreply.github.com>
2025-02-03 15:30:54 -0400

    Adding the "add" function

--- LinkListClass.java
+++ LinkListClass.java
@@ -3,4 +3,31 @@ public class  LinkListClass{
   public LinkListClass(){
     this.head = null;
   }
+  public void add(int data, int pos){
+    Node newNode = new Node(data);
+    if(pos==0){
+      newNode.next = this.head;
+      this.head = newNode;
+    }else{
+      Node current = this.head;
+      for(int i = 0; i < pos-1; i++){
+        if(current!=null){
+          current = current.next;
+        }else{
+          System.out.println("Index out of range.");
+        }
+      }
+      if(current!=null){
+        newNode.next = current.next;
+        current.next = newNode;
+      }
+    }
+}
+class Node{
+  int data;
+  Node next;
+  public Node(int data){
+    this.data = data;
+    this.next = null;
+  }
 }
